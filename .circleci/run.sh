07ea5a37d471b148f7ccda2af3d9001d8ed2ae83
Maxim Malyk <malyk.maxim@gmail.com>
2018-12-09 14:39:22 +0200

    Update config

--- .circleci/run.sh
+++ .circleci/run.sh
@@ -4,7 +4,7 @@ TOKEN=52b163a687a29f0b44b910b7da3dda671d5a84c7
 
 curl --user $TOKEN: \
     --request POST \
-    --form revision=991808f04cadf7652cc866a0b112df490cbb9a14 \
+    --form revision=6b1fc52a5c8486e2313ed85065a2d1a136739a0b \
     --form config=@config.yml \
     --form notify=false \
     https://circleci.com/api/v1.1/project/github/vipaction/codeid-ci-demo/tree/master
