c5ec1d673c3db757a3525fc1420c42c84276421b
Maxim Malyk <malyk.maxim@gmail.com>
2018-12-09 14:54:10 +0200

    Updated jobs

--- .circleci/run.sh
+++ .circleci/run.sh
@@ -4,7 +4,7 @@ TOKEN=52b163a687a29f0b44b910b7da3dda671d5a84c7
 
 curl --user $TOKEN: \
     --request POST \
-    --form revision=6b1fc52a5c8486e2313ed85065a2d1a136739a0b \
+    --form revision=f5ec0578fcc0c4c42d2036a6c2dba49a66b86e91 \
     --form config=@config.yml \
     --form notify=false \
     https://circleci.com/api/v1.1/project/github/vipaction/codeid-ci-demo/tree/master
